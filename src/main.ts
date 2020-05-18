773c32bb97bbcb14de921e1da17c0b92e2843604
Peter Gasparik <gasparik.pe@gmail.com>
2020-05-18 20:32:04 +0200

    add validation library

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,20 @@ import { LogOutsourcedApi, LogOutsourcedHttpApi } from './lib/log-outsourced-api
 import { HandlerModule } from './module/handler'
 import { ApiModule } from './module/api'
 import { ListenerModule } from './module/listener'
+import { ExtendableValidatorBuilder, Validator } from './lib/validator'
+import { BasicValidatorRuleProvide } from './lib/validator/basic/BasicValidatorRuleProvider'
+import { StringRuleProvider } from './lib/validator/string/StringRuleProvider'
 
 const host: string = process.env.VUE_APP_LOG_OUTSOURCED_API_HOST
 const httpApi: LogOutsourcedApi = new LogOutsourcedHttpApi(host)
+const validatorBuilder: ExtendableValidatorBuilder = new ExtendableValidatorBuilder()
+validatorBuilder.extend(new BasicValidatorRuleProvide())
+validatorBuilder.extend(new StringRuleProvider())
 
 const framework: ModularFramework = new ModularFramework([
     new SceneModule(),
     new AlertModule(),
-    new ProjectModule(httpApi.projects()),
+    new ProjectModule(httpApi.projects(), validatorBuilder),
     new HandlerModule(httpApi.handlers()),
     new ListenerModule(httpApi.listeners()),
     new ApiModule(host)
