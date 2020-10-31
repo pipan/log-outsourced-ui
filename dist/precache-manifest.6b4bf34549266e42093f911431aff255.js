5c03db6649e09a29a91a235fb796ece7ee8ee196
Peter Gasparik <gasparik.pe@gmail.com>
2020-10-31 16:13:08 +0100

    build v0.2.4

---
+++ dist/precache-manifest.6b4bf34549266e42093f911431aff255.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7c0d7090d3247bc91ed91cd165017d",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "fc7b41b29ae6085aef45741aeca3d529",
+    "url": "/android-chrome-384x384.png"
+  },
+  {
+    "revision": "7beac3fa0106f0e858c0c41a2729d62b",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "61bfd064535af0c276bb63b3fd579733",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "1488ab6a391ec7b0f42e02708e7d8899",
+    "url": "/config.json"
+  },
+  {
+    "revision": "747b5d136708f34f58f8",
+    "url": "/css/app.dae2a96b.css"
+  },
+  {
+    "revision": "b1d571b8a325e0ef6b3c16009baa8f4e",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "cfc2abaf44e30001af0d2b3286961710",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "c7e37e87f9e1401ae9eeba1af94317db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747b5d136708f34f58f8",
+    "url": "/js/app.c46250f3.js"
+  },
+  {
+    "revision": "fbe36e7bbb1281753651",
+    "url": "/js/chunk-vendors.de91607a.js"
+  },
+  {
+    "revision": "4fb6feeb25c9568554a67245014ea8ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "14cafd10983ed518a383bdef17d057c4",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3efa0b75c1d05165d93d4fe50627f5a6",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "2672444b686b372798f73a92b09d685c",
+    "url": "/site.webmanifest"
+  }
+]);
